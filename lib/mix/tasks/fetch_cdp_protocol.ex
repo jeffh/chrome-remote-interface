1c150034a81151f7cb37dd8b7698ca34528caaf9
Jeff Hui <jeff@jeffhui.net>
2024-11-29 18:33:44 -0800

    update protocol fetcher to pull+merge multiple files

--- lib/mix/tasks/fetch_cdp_protocol.ex
+++ lib/mix/tasks/fetch_cdp_protocol.ex
@@ -11,26 +11,29 @@ defmodule Mix.Tasks.FetchCdpProtocol do
 
   @protocol_sources %{
     "1-2" => %{
-      url:
-        "https://raw.githubusercontent.com/ChromeDevTools/debugger-protocol-viewer/refs/heads/master/pages/_data/1-2.json",
+      urls:
+        ["https://raw.githubusercontent.com/ChromeDevTools/debugger-protocol-viewer/refs/heads/master/pages/_data/1-2.json"],
       output: "priv/1-2/protocol.json"
     },
     "1-3" => %{
-      url:
-        "https://raw.githubusercontent.com/ChromeDevTools/debugger-protocol-viewer/refs/heads/master/pages/_data/1-3.json",
+      urls:
+        ["https://raw.githubusercontent.com/ChromeDevTools/debugger-protocol-viewer/refs/heads/master/pages/_data/1-3.json"],
       output: "priv/1-3/protocol.json"
     },
     "tot" => %{
-      url:
-        "https://raw.githubusercontent.com/ChromeDevTools/devtools-protocol/refs/heads/master/json/browser_protocol.json",
+      urls:
+        [
+          "https://raw.githubusercontent.com/ChromeDevTools/devtools-protocol/refs/heads/master/json/browser_protocol.json",
+          "https://raw.githubusercontent.com/ChromeDevTools/devtools-protocol/refs/heads/master/json/js_protocol.json",
+        ],
       output: "priv/tot/protocol.json"
     }
   }
 
-  @temporary_file "protocol.json"
-
   @impl true
   def run(_) do
+    Application.ensure_all_started(:hackney)
+    Application.ensure_all_started(:jason)
     Map.keys(@protocol_sources)
     |> Enum.each(&fetch_protocol/1)
   end
@@ -38,17 +41,24 @@ defmodule Mix.Tasks.FetchCdpProtocol do
   def fetch_protocol(version) do
     protocol_source = Map.fetch!(@protocol_sources, version)
 
-    cmd!("wget #{protocol_source.url}")
-    File.rename(@temporary_file, protocol_source.output)
-  end
+    specs = protocol_source.urls
+    |> Stream.map(fn url ->
+        with {:ok, 200, _, client_ref} <- :hackney.request(:get, url, [], <<>>, []),
+             {:ok, body} <- :hackney.body(client_ref),
+             {:ok, parsed_body} <- Jason.decode(body) do
+          parsed_body
+        else
+          error -> raise "failed to fetch: #{inspect(error)}"
+        end
+    end)
 
-  defp cmd!(cmd) do
-    Mix.shell().info([:magenta, "Running: #{cmd}"])
+    [spec] = Enum.take(specs, 1)
 
-    exit_status = Mix.shell().cmd(cmd)
+    additional_domains = specs
+      |> Stream.drop(1)
+      |> Stream.flat_map(fn other_spec -> other_spec["domains"] end)
 
-    if exit_status != 0 do
-      Mix.raise("Non-zero result (#{exit_status}) from command: #{cmd}")
-    end
+    spec = %{spec | "domains" => Enum.concat(spec["domains"], additional_domains)}
+    File.write!(protocol_source.output, Jason.encode!(spec, pretty: true))
   end
 end
